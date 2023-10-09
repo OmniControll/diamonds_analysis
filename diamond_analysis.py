90b2d61eb2c683245312899327ffe5d1b0ee91ad
OmniControll <qais13@hotmail.com>
2023-10-10 01:05:10 +0200

    added distributions, and comments

--- diamond_analysis.py
+++ diamond_analysis.py
@@ -7,6 +7,9 @@ import matplotlib.pyplot as plt
 
 diamonds = pd.read_csv('diamonds.csv')
 
+# Business Question: 
+# How do the physical attributes and quality grades of a diamond relate to its market price, 
+# can we accurately predict the price of a diamond based on these features?"
 
 # so we have 10 columns ; 8 numeric and 2 categorical. [carat, depth, table, price, x, y, z] are numeric and [cut, color, clarity] are categorical.
 
@@ -18,7 +21,6 @@ diamonds = pd.read_csv('diamonds.csv')
 # the density of the diamond is the mass divided by the volume.
 # the mass of the diamond is the density times the volume.
 # the volume of the diamond is the product of x, y and z.
-
 
 # So what do we want to know about this data?
 
@@ -51,8 +53,54 @@ plt.show()
 #histogram of carat
 sns.histplot(diamonds['carat'], bins=50)
 plt.xticks(np.arange(0, 6, step=0.5))  # steps of 0.5 carat
-plt.yticks(np.arange(0, 10000, step=1000))
+plt.yticks(np.arange(0, 15000, step=3000))
 plt.xlabel('Carat')
 plt.ylabel('Nr of Diamonds')
 plt.show()
+
+#there are some outliers in the carat feature. we can remove them by removing the rows with carat > 3
+#diamonds = diamonds[diamonds['carat'] < 3]
+#But considering we're dealing with luxury products, it's not surprising to have some outliers representing rare cases. so we'll keep them.
+#in linear regression, outliers can affect the model's performance. so we'll apply random forest regressor instead later on.
+
+#distribution of depth
+sns.histplot(diamonds['depth'], bins=50)
+plt.xticks(np.arange(50, 80, step=5))  # steps of 5
+plt.yticks(np.arange(0, 15000, step=2500))
+plt.xlabel('Depth')
+plt.ylabel('Nr of Diamonds')
+plt.show()
+
+#distribution of table
+sns.histplot(diamonds['table'], bins=35)
+plt.xticks(np.arange(40, 70, step=5))  # steps of 5
+plt.yticks(np.arange(0, 20000, step=3000))
+plt.xlabel('Table')
+plt.ylabel('Nr of Diamonds')
+plt.show()
+
+#distribution of cut
+sns.histplot(diamonds['cut'], bins=50)
+plt.xticks(np.arange(0, 6, step=1))  # steps of 1
+plt.yticks(np.arange(0, 25000, step=5000))
+plt.xlabel('Cut')
+plt.ylabel('Nr of Diamonds')
+plt.show()
+
+#distribution of clarity
+sns.histplot(diamonds['clarity'], bins=50)
+plt.xticks(np.arange(0, 9, step=1))  # steps of 1
+plt.yticks(np.arange(0, 15000, step=3000))
+plt.xlabel('Clarity')
+plt.ylabel('Nr of Diamonds')
+plt.show()
+
+#distribution of color
+sns.histplot(diamonds['color'], bins=50)
+plt.xticks(np.arange(0, 8, step=1))  # steps of 1
+plt.yticks(np.arange(0, 15000, step=1500))
+plt.xlabel('Color')
+plt.ylabel('Nr of Diamonds')
+plt.show()
+
 
